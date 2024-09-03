4e1f90287baa9e9935f39df81ee7fcaf3db51269
zahradnik33 <130229378+zahradnik33@users.noreply.github.com>
2024-09-03 16:12:53 +0200

    Add files via upload

---
+++ dbcomps.sql
@@ -0,0 +1,12 @@
+set pagesize 0
+set heading on
+col comp_id format a10
+col comp_name format a30
+col version format a10
+col status format a8
+select substr(comp_id,1,15) comp_id
+,      substr(comp_name,1,30) comp_name
+,      substr(version,1,10) version
+,      status
+from dba_registry
+/
